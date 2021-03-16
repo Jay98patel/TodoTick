d64538dcdb8aafbeb7bb52b790f65e1f68b34e94
KevangPatel <kevangpatel@gmail.com>
2021-03-16 20:24:42 +0530

    Crud services

---
+++ src/app/todo/services/todo.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Todo } from '../todo.model';
+
+@Injectable()
+export class TodoService {
+  private baseURL: string;
+
+  
+
+  constructor(private http: HttpClient) {
+    this.baseURL = environment.apiUrl;
+  }
+
+  getAllTodoList(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.baseURL);
+  }
+
+  createTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.baseURL, todo);
+  }
+
+  updateTodoList(todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(this.baseURL + `/` + todo.id, todo);
+  }
+
+  deleteTodoItem(id: Number): Observable<Todo> {
+    return this.http.delete<Todo>(this.baseURL + `/` + id);
+  }
+
+}
