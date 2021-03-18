7d53bcc761958fb81c508d482c4e8ff144b0254d
KevangPatel <kevangpatel@gmail.com>
2021-03-18 11:49:53 +0530

    routing of home comp.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
   {   
     path: 'todo',
     loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule)
+  },  
+  {   
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
   }
 ];
 
