9b4ac12796e409f0ac6726f648056f28ae80d991
KevangPatel <kevangpatel@gmail.com>
2021-03-23 09:19:36 +0530

    userform form document

--- src/app/users/user-form/user-form.component.ts
+++ src/app/users/user-form/user-form.component.ts
@@ -7,7 +7,8 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
-  userForm: FormGroup
+  userForm: FormGroup;
+  imageUpload: File;
   constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
@@ -20,11 +21,12 @@ export class UserFormComponent implements OnInit {
     return this.fb.group({
       id: [''],
       firstName: ['', Validators.required],
-      userName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(5),Validators.pattern(lowerCase)]],
+      userName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(5), Validators.pattern(lowerCase)]],
       role: ['0', Validators.required],
       workExpiriences: this.fb.array([
         this.initializeWorExp()
       ]),
+      acceptTerms: [false, Validators.required],
       userDate: ['', Validators.required],
     });
   }
@@ -55,8 +57,22 @@ export class UserFormComponent implements OnInit {
     console.log(this.workArrayControl().length);
   }
 
+  onImageChanged(e) {
+    let imageReader = new FileReader();
+    let image = e.target.files[0];
+    if (e.target.files.length > 0) {
+      const file = e.target.files;
+      this.imageUpload = file;
+    }
+  }
+
   submitUser() {
-    console.log(this.userForm.value)
+    if (this.userForm.invalid) {
+      console.log(this.userForm.invalid)
+    }
+    else {
+      console.log(this.userForm.value)
+    }
   }
 
 }
