e4854d93a7e2bf56f0abb5c869b26c697ecd9019
KvngPatel <kevangpatel@gmail.com>
2021-04-05 12:27:21 +0530

    pagination

--- src/app/weather/components/weather-list/weather-list.component.ts
+++ src/app/weather/components/weather-list/weather-list.component.ts
@@ -1,8 +1,10 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Weather } from '../../model/weather.model';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDataSource } from '../../dataSource/weatherDataSource';
+import { MatPaginator } from '@angular/material/paginator';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-weather-list',
@@ -10,10 +12,12 @@ import { WeatherDataSource } from '../../dataSource/weatherDataSource';
   styleUrls: ['./weather-list.component.scss']
 })
 export class WeatherListComponent implements OnInit, AfterViewInit {
-  weather: Weather[];
+  weatherList: Weather[];
   weatherSource: WeatherDataSource;
   error: string;
   displayedColumns = ['id', 'cityName', 'State', 'country', 'windSpeed', 'temparature', 'humidity', 'region'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor(private route: ActivatedRoute, private router: Router, private weatherService: WeatherService) { }
 
   ngOnInit() {
@@ -21,11 +25,13 @@ export class WeatherListComponent implements OnInit, AfterViewInit {
   }
 
   getWeather() {
-    this.route.data.subscribe((weather: { weatherList: Weather[] }) => {
-      this.weather = weather.weatherList;
-      this.weatherSource = new WeatherDataSource(this.weatherService);
-      this.weatherSource.loadWeather('', 'asc', 0, 3);
-    },
+    this.route.data.subscribe(
+      (weather: { weatherList: Weather[] }) => {
+        this.weatherList = weather.weatherList;
+        this.weatherSource = new WeatherDataSource(this.weatherService);
+        this.weatherSource.loadWeather('', 'asc', 0, 3);
+        console.log(this.weatherList?.length)
+      },
       (error) => {
         this.error = error;
         console.log("error in loading Weather");
@@ -33,7 +39,18 @@ export class WeatherListComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-
+    this.paginator.page
+      .pipe(
+        tap(() => this.loadWeatherPage())
+      )
+      .subscribe();
   }
 
+  loadWeatherPage() {
+    this.weatherSource.loadWeather(
+      '',
+      'asc',
+      this.paginator.pageIndex,
+      this.paginator.pageSize);
+  }
 }
