7ab43485a60ce35c668a6bc65a18c4afa28eae09
KvngPatel <kevangpatel@gmail.com>
2021-04-19 13:32:42 +0530

    implemented life-cycle-hook method in grandparent component

--- src/app/life-cycle-hook/life-cycle-hook.component.ts
+++ src/app/life-cycle-hook/life-cycle-hook.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-life-cycle-hook',
   templateUrl: './life-cycle-hook.component.html',
   styleUrls: ['./life-cycle-hook.component.scss']
 })
-export class LifeCycleHookComponent implements OnInit {
+export class LifeCycleHookComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  constructor() {
+    console.log("Grand-Parent-component: Constructor");
+  }
 
-  constructor() { }
+  ngOnChanges() {
+    console.log("Grand-Parent-component: OnChanges");
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    console.log("Grand-Parent-component: OnInit");
+  }
+
+  ngDoCheck() {
+    console.log("Grand-Parent-component: DoCheck");
+  }
+
+  ngAfterContentInit() {
+    console.log("Grand-Parent-component: AfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("Grand-Parent-component:AfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("Grand-Parent-component:AfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("Grand-Parent-component:AfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("Grand-Parent-component:OnDestroy");
   }
 
 }
--- src/app/life-cycle-hook/life-cycle-hook.module.ts
+++ src/app/life-cycle-hook/life-cycle-hook.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LifeCycleHookRoutingModule } from './life-cycle-hook-routing.module';
+import { ParentComponent } from './components/parent/parent.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ParentComponent],
   imports: [
     CommonModule,
     LifeCycleHookRoutingModule
